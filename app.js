bce26fe9da493bd07ad5a2805425875404e82500
Luis Francisco <lfbaroni@gmail.com>
2023-10-28 11:37:26 -0700

    adds size text

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 const sharp = require("sharp");
+const { createCanvas, loadImage } = require("canvas");
 
 const app = express();
 app.use(express.static("public"));
@@ -16,11 +17,31 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+async function drawTextOnImage(imageBuffer, text, leftPosition, topPosition, resizedProductHeight) {
+  const image = await loadImage(imageBuffer);
+  const canvas = createCanvas(image.width, image.height);
+  const ctx = canvas.getContext("2d");
+  ctx.drawImage(image, 0, 0);
+
+  const textWidth = ctx.measureText(text).width;
+
+  const textLeftPosition = leftPosition - textWidth - 240;
+  const textTopPosition = topPosition + ((resizedProductHeight / 2) + 20) ;  // Centered vertically
+
+  ctx.fillStyle = "black";
+  ctx.strokeStyle = "white";
+  ctx.lineWidth = 18;
+  ctx.font = 'Arial bold 80px sans-serif';
+  ctx.strokeText(text, textLeftPosition, textTopPosition);
+  ctx.fillText(text, textLeftPosition, textTopPosition);
+
+  return canvas.toBuffer();
+}
+
 app.post("/generate", upload.single("productImage"), async (req, res) => {
   try {
     const uploadedImagePath = path.join(__dirname, "uploads", req.file.filename);
     const referenceImagePath = path.join(__dirname, "reference_images", `${req.body.referenceImage}.png`);
-    const outputImagePath = path.join(__dirname, "uploads", "output_" + req.file.filename);
 
     const referenceSizeCm = 90;
     const productSizeCm = parseFloat(req.body.productHeight);
@@ -33,32 +54,33 @@ app.post("/generate", upload.single("productImage"), async (req, res) => {
       height: Math.round(2000 * scale)
     }).toBuffer();
 
-    const resizedProductImageMetadata = await sharp(resizedProductImageBuffer).metadata();
-    const resizedProductWidth = resizedProductImageMetadata.width;
-
     const referenceImageMetadata = await referenceImage.metadata();
     const refWidth = referenceImageMetadata.width;
     const refHeight = referenceImageMetadata.height;
 
-    const leftPosition = Math.round((refWidth - resizedProductWidth) / 2);
-    const topPosition = (refHeight- 250) - Math.round(2000 * scale);
+    const resizedProductImageMetadata = await sharp(resizedProductImageBuffer).metadata();
+    const resizedProductWidth = resizedProductImageMetadata.width;
+    const resizedProductHeight = Math.round(2000 * scale);
 
-    referenceImage.composite([{
+
+    const leftPosition = Math.round((refWidth - resizedProductWidth) / 2);
+    const topPosition = (refHeight - 250) - Math.round(2000 * scale);
+
+    const outputBuffer = await referenceImage.composite([{
       input: resizedProductImageBuffer,
       top: topPosition,
       left: leftPosition
-    }]).toFile(outputImagePath, (err, info) => {
-      if (err) {
-        console.error("Composite error:", err);
-        return res.status(500).send("Internal Server Error");
-      }
-      res.sendFile(outputImagePath, () => {
-        fs.unlinkSync(uploadedImagePath);
-        fs.unlinkSync(outputImagePath);
-      });
-    });
+    }]).toBuffer();
+
+    const outputWithText = await drawTextOnImage(outputBuffer, `${req.body.productHeight} cm`, leftPosition, topPosition, resizedProductHeight);
+
+
+    res.setHeader("Content-Type", "image/png");
+    res.send(outputWithText);
+
+    fs.unlinkSync(uploadedImagePath);
   } catch (error) {
-    console.error("An internal error occurred:", error);
+    console.error("An error occurred:", error);
     res.status(500).send("Internal Server Error");
   }
 });
